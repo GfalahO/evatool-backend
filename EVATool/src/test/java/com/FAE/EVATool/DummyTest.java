4a22f576bde5aaa218796cf9963b166ccca5baea
Henning Buhl <henning.buhl@yahoo.de>
2021-01-15 18:11:11 +0100

    Testing Action

--- EVATool/src/test/java/com/FAE/EVATool/DummyTest.java
+++ EVATool/src/test/java/com/FAE/EVATool/DummyTest.java
@@ -1,12 +1,13 @@
+package com.FAE.EVATool;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 public class DummyTest {
 
-
     @Test
     public void testAlwaysPasses(){
-
         Assertions.assertEquals(1, 1);
     }
 }
