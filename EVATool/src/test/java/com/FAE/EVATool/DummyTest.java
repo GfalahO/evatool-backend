fd876eeff494839b80f9526594841d29c6cbaeed
Henning Buhl <henning.buhl@yahoo.de>
2021-01-15 16:59:13 +0100

    Added GitHub Action for test

---
+++ EVATool/src/test/java/com/FAE/EVATool/DummyTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+
+
+    @Test
+    public void testAlwaysPasses(){
+
+        Assertions.assertEquals(1, 1);
+    }
+}
