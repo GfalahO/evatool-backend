54c80278158e449024bb446d78c1e8250603ea7f
Henning Buhl <henning.buhl@yahoo.de>
2021-01-27 11:32:00 +0100

    Reworked test

--- EVATool/impact/src/test/java/com/evatool/impact/persistence/repository/DimensionRepositoryTest.java
+++ EVATool/impact/src/test/java/com/evatool/impact/persistence/repository/DimensionRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.evatool.impact.persistence.repository;
 
 import com.evatool.impact.persistence.TestDataGenerator;
+import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 //@RunWith(SpringJUnit4ClassRunner.class) // Works with and without.
 @DataJpaTest
@@ -43,17 +46,18 @@ public class DimensionRepositoryTest {
         // given
         var dimension = TestDataGenerator.getDimension();
         dimensionRepository.save(dimension);
-        String oldName = dimension.getName();
-        String newName = "care";
+        var oldName = dimension.getName();
+        var newName = "care";
 
         // when
         dimension.setName(newName);
-        dimensionRepository.save(dimension); // This line can be commented and the test still passes
-                                                   // Why does save not need to be called?
+        //dimensionRepository.save(dimension); // This line can be commented and the test still passes
+                                               // Why does save not need to be called?
+                                               // CrudRepository saved changed with setter methods automatically.
         var changedDimension = dimensionRepository.findById(dimension.getId()).orElse(null);
 
         // then
-        Assert.assertEquals(newName, changedDimension.getName());
+        assertThat(changedDimension.getName()).isEqualTo(newName);
     }
 
     @Test
