eb4f00d1c5338076b590f984dec508a3698c4939
Henning Buhl <henning.buhl@yahoo.de>
2021-01-27 17:15:50 +0100

    Removed java doc

--- EVATool/impact/src/main/java/com/evatool/impact/persistence/entity/Dimension.java
+++ EVATool/impact/src/main/java/com/evatool/impact/persistence/entity/Dimension.java
@@ -6,15 +6,9 @@ import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
-/**
- * A class representing the entity and object Dimension.
- */
 @Entity(name = "DIMENSION")
 @Table(name = "DIMENSION")
 public class Dimension {
-    /**
-     * The id of the object.
-     */
     @Getter
     @Id
     @GeneratedValue(generator = "system-uuid")
@@ -22,41 +16,25 @@ public class Dimension {
     @Column(name = "ID")
     private String id;
 
-    /**
-     * The name of the object.
-     */
     @Getter
     @NotNull
     @Column(name = "NAME", unique = true)
     private String name;
 
-    /**
-     * The description of the object.
-     */
     @Getter
     @NotNull
     @Column(name = "DESCRIPTION")
     private String description;
 
-    /**
-     *
-     */
     public Dimension() {
 
     }
 
-    /**
-     * @param name:        The name of the object
-     * @param description: The description of the object.
-     */
     public Dimension(String name, String description) {
         this.setName(name);
         this.setDescription(description);
     }
 
-    /**
-     * @return a string describing the object.
-     */
     @Override
     public String toString() {
         return String.format(
@@ -64,10 +42,6 @@ public class Dimension {
                 this.id, this.name, this.description);
     }
 
-    /**
-     * @param name: The new name of the object
-     * @throws IllegalArgumentException if argument is null.
-     */
     public void setName(String name) throws IllegalArgumentException {
         if (name == null) {
             throw new IllegalArgumentException("Name cannot be null.");
@@ -75,10 +49,6 @@ public class Dimension {
         this.name = name;
     }
 
-    /**
-     * @param description: The new description of the object
-     * @throws IllegalArgumentException if argument is null.
-     */
     public void setDescription(String description) throws IllegalArgumentException {
         if (description == null) {
             throw new IllegalArgumentException("Description cannot be null.");
