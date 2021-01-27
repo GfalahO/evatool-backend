fe8c65944bebe6dc10a7924dab0b46472d47455e
Henning Buhl <henning.buhl@yahoo.de>
2021-01-27 11:32:16 +0100

    Added async event config

---
+++ EVATool/impact/src/main/java/com/evatool/impact/persistence/event/AsynchronousSpringEventsConfig.java
@@ -0,0 +1,19 @@
+package com.evatool.impact.persistence.event;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+// This class causes test fails...
+// TODO: Make tests work with async config
+//@Configuration // This line is the problem.
+public class AsynchronousSpringEventsConfig {
+    @Bean(name = "applicationEventMulticaster")
+    public ApplicationEventMulticaster simpleApplicationEventMulticaster() {
+        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
+        eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        return eventMulticaster;
+    }
+}
