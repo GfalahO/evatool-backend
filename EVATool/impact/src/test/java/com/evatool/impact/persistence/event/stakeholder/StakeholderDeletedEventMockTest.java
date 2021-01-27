f9c395a2e137dcf9537b8c915be8c4c64991bcd7
Henning Buhl <henning.buhl@yahoo.de>
2021-01-27 20:44:54 +0100

    Added StakeholderDeletedEvent tests

---
+++ EVATool/impact/src/test/java/com/evatool/impact/persistence/event/stakeholder/StakeholderDeletedEventMockTest.java
@@ -0,0 +1,33 @@
+package com.evatool.impact.persistence.event.stakeholder;
+
+import com.evatool.impact.persistence.TestDataGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+public class StakeholderDeletedEventMockTest {
+    @Autowired
+    private StakeholderDeletedEventPublisher publisher;
+
+    @MockBean
+    private StakeholderDeletedEventListener listener;
+
+    @Test
+    public void testOnApplicationEvent_PublishEvent_ReceivePublishedEventOnce() throws InterruptedException {
+        // given
+        var stakeholder = TestDataGenerator.getStakeholder();
+
+        // when
+        publisher.onStakeholderDeleted(stakeholder);
+        Thread.sleep(100);
+
+        // then
+        verify(listener, times(1)).onApplicationEvent(any(StakeholderDeletedEvent.class));
+    }
+}
---
+++ EVATool/impact/src/test/java/com/evatool/impact/persistence/event/stakeholder/StakeholderDeletedEventTest.java
@@ -0,0 +1,36 @@
+package com.evatool.impact.persistence.event.stakeholder;
+
+import com.evatool.impact.persistence.repository.StakeholderRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.evatool.impact.persistence.TestDataGenerator.getStakeholder;
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class StakeholderDeletedEventTest {
+    @Autowired
+    private StakeholderRepository stakeholderRepository;
+
+    @Autowired
+    private StakeholderDeletedEventPublisher publisher;
+
+    @Autowired
+    private StakeholderDeletedEventListener listener;
+
+    @Test
+    public void testOnApplicationEvent_PublishEvent_ReturnNull() throws InterruptedException {
+        // given
+        var stakeholder = getStakeholder();
+        stakeholderRepository.save(stakeholder);
+
+        // when
+        publisher.onStakeholderDeleted(stakeholder);
+        Thread.sleep(100);
+        var found = stakeholderRepository.findById(stakeholder.getId()).orElse(null);
+
+        // then
+        assertThat(found).isNull();
+    }
+}
