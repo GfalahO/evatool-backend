62f782b39dfcebb6110613d571f478dd37215acc
Henning Buhl <henning.buhl@yahoo.de>
2021-01-29 23:37:06 +0100

    Added setId, toDto and fromDto

--- evatool/impact/src/main/java/com/evatool/impact/persistence/entity/Stakeholder.java
+++ evatool/impact/src/main/java/com/evatool/impact/persistence/entity/Stakeholder.java
@@ -1,9 +1,12 @@
 package com.evatool.impact.persistence.entity;
 
+import com.evatool.impact.common.dto.StakeholderDto;
 import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.util.UUID;
 
 @Entity(name = "STAKEHOLDER")
 @Table(name = "STAKEHOLDER")
@@ -24,7 +27,13 @@ public class Stakeholder {
     }
 
     public Stakeholder(String name) {
+        this();
         this.setName(name);
+    }
+
+    private Stakeholder(String id, String name) {
+        this(name);
+        this.id = id;
     }
 
     @Override
@@ -34,10 +43,43 @@ public class Stakeholder {
                 this.id, this.name);
     }
 
+    public void setId(String id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Id cannot be null.");
+        }
+        if (this.id != null) {
+            throw new IllegalArgumentException("Cannot set existing id to null.");
+        }
+        //try {
+        UUID.fromString(id);
+        //} catch (IllegalArgumentException exception) {
+        //    throw new IllegalArgumentException("Id must be a valid UUID.");
+        //}
+        this.id = id;
+    }
+
     public void setName(String name) {
         if (name == null) {
             throw new IllegalArgumentException("Name cannot be null.");
         }
         this.name = name;
+    }
+
+    public StakeholderDto toDto() {
+        var stakeholderDto = new StakeholderDto();
+
+        stakeholderDto.setId(this.getId());
+        stakeholderDto.setName(this.getName());
+
+        return stakeholderDto;
+    }
+
+    public static Stakeholder fromDto(StakeholderDto stakeholderDto) {
+        var stakeholder = new Stakeholder();
+
+        stakeholder.id = stakeholderDto.getId();
+        stakeholder.setName(stakeholderDto.getName());
+
+        return stakeholder;
     }
 }
