68288526a01de11af75e3a45c3e193ccd6440235
Henning Buhl <henning.buhl@yahoo.de>
2021-01-30 11:00:00 +0100

    Create DimensionDto.java

---
+++ evatool/impact/src/main/java/com/evatool/impact/common/dto/DimensionDto.java
@@ -0,0 +1,27 @@
+package com.evatool.impact.common.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class DimensionDto {
+    @Getter
+    @Setter
+    private String id;
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    private String description;
+
+    @Override
+    public String toString() {
+        return "DimensionDto{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
