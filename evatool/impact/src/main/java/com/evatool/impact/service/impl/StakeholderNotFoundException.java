60b7a64007292d087000d1bf5eff6950cb7dacd6
Henning Buhl <henning.buhl@yahoo.de>
2021-01-30 09:32:48 +0100

    Added HttpStatus.NOT_FOUND return code

--- evatool/impact/src/main/java/com/evatool/impact/service/impl/StakeholderNotFoundException.java
+++ evatool/impact/src/main/java/com/evatool/impact/service/impl/StakeholderNotFoundException.java
@@ -1,5 +1,9 @@
 package com.evatool.impact.service.impl;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class StakeholderNotFoundException extends Exception {
     public StakeholderNotFoundException(String message) {
         super(message);
