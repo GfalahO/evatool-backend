c83f7faef2e916a333a94cdc8b742f855a53dca8
Henning Buhl <henning.buhl@yahoo.de>
2021-02-01 13:33:34 +0100

    Added and reworked exceptions

--- evatool/impact/src/main/java/com/evatool/impact/exception/EntityNotFoundException.java
+++ evatool/impact/src/main/java/com/evatool/impact/exception/EntityNotFoundException.java
@@ -1,4 +1,4 @@
-package com.evatool.impact.service;
+package com.evatool.impact.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
