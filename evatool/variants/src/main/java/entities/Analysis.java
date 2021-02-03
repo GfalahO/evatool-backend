2b60cae40a674b83bd716ddf781a3b70fe602faa
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-02-03 14:24:21 +0100

    Added initial entities

---
+++ evatool/variants/src/main/java/entities/Analysis.java
@@ -0,0 +1,17 @@
+package entities;
+
+import lombok.Getter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity(name = "ANALYSIS")
+@Getter
+public class Analysis {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+
+    private String title;
+}
---
+++ evatool/variants/src/main/java/entities/Requirement.java
@@ -0,0 +1,19 @@
+package entities;
+
+import lombok.Getter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "REQUIREMENT")
+@Getter
+public class Requirement {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+
+    private String title;
+}
