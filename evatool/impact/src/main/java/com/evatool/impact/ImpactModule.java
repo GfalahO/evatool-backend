0419f99b27bb4b701b8b349d7cc81e20d49107b5
Henning Buhl <henning.buhl@yahoo.de>
2021-02-03 22:44:38 +0100

    Added TODO

--- evatool/impact/src/main/java/com/evatool/impact/ImpactModule.java
+++ evatool/impact/src/main/java/com/evatool/impact/ImpactModule.java
@@ -3,6 +3,8 @@ package com.evatool.impact;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// TODO: [hbuhl] make all tests more entity agnostic (variable names)
+
 @SpringBootApplication
 public class ImpactModule {
     public static void main(String[] args) {
