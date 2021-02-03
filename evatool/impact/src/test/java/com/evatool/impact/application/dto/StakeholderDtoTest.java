e99bcd340c9bd5dedd5b9b42336857c0fa1565b9
Henning Buhl <henning.buhl@yahoo.de>
2021-02-03 21:30:51 +0100

    Deleted test

--- evatool/impact/src/test/java/com/evatool/impact/application/dto/StakeholderDtoTest.java
+++ evatool/impact/src/test/java/com/evatool/impact/application/dto/StakeholderDtoTest.java
@@ -2,16 +2,6 @@ package com.evatool.impact.application.dto;
 
 import org.junit.Test;
 
-// TODO [hbuhl] delete this;
 public class StakeholderDtoTest {
-    @Test
-    public void testToString_DefaultObject_DoNotThrowException() {
-        // given
-        var stakeholderDto = new StakeholderDto();
 
-        // when
-
-        // then
-        stakeholderDto.toString();
-    }
 }
