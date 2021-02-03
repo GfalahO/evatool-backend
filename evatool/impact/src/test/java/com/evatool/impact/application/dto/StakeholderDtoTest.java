1705b63b6c27e399f4b094a5ee35950cabd24f65
Henning Buhl <henning.buhl@yahoo.de>
2021-02-03 21:25:37 +0100

    Did TODO

--- evatool/impact/src/test/java/com/evatool/impact/application/dto/StakeholderDtoTest.java
+++ evatool/impact/src/test/java/com/evatool/impact/application/dto/StakeholderDtoTest.java
@@ -1,9 +1,8 @@
 package com.evatool.impact.application.dto;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 // TODO [hbuhl] delete this;
-// TODO [hbuhl] why .jupiter.api.Test is used and not org.junit.Test?
 public class StakeholderDtoTest {
     @Test
     public void testToString_DefaultObject_DoNotThrowException() {
