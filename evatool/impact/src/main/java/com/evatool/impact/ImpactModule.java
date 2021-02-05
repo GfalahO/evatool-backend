fccfa76cc56cd4a535ddfbc7dc5c90876826b021
Henning Buhl <henning.buhl@yahoo.de>
2021-02-05 15:47:45 +0100

    Added TODOs

--- evatool/impact/src/main/java/com/evatool/impact/ImpactModule.java
+++ evatool/impact/src/main/java/com/evatool/impact/ImpactModule.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // TODO: [hbuhl] make all tests more entity agnostic (variable names)
+// TODO: Swagger doc
+// TODO: What about other documentation?
+// TODO: API Specification
+// TODO: Eventing Specification
+// TODO: Use Rest level 3
 
 @SpringBootApplication
 public class ImpactModule {
