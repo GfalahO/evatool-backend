fce87a5b96d9cbc2823e942d66f5ec4ac1b79e9f
Henning Buhl <henning.buhl@yahoo.de>
2021-02-05 11:14:05 +0100

    Fixed base uri

--- evatool/impact/src/main/java/com/evatool/impact/ModuleSettings.java
+++ evatool/impact/src/main/java/com/evatool/impact/ModuleSettings.java
@@ -2,5 +2,5 @@ package com.evatool.impact;
 
 public class ModuleSettings {
     public static final String MODULE_NAME = "impact";
-    public static final String BASE_URI = "/" + MODULE_NAME + "/" + MODULE_NAME;
+    public static final String BASE_URI = "/" + MODULE_NAME;
 }
