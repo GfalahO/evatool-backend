a0df314a194bb89bb12b46e23a4b3cf77836bbbc
Henning Buhl <henning.buhl@yahoo.de>
2021-02-07 22:02:43 +0100

    Update ImpactModule.java

--- impact/src/main/java/com/evatool/impact/ImpactModule.java
+++ impact/src/main/java/com/evatool/impact/ImpactModule.java
@@ -4,19 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // Features:
-    // TODO Better error handling for possible rest calls
-    // TODO Events
-    // TODO More Impact URIs
+// TODO Better error handling for possible rest calls
+// TODO Events
+// TODO More Impact URIs
 
 // Tests:
-    // TODO [tzaika] Implement Dimension Tests
-    // TODO [tzaika] Implement Impact Tests
-    // TODO Cover all scenarios with Tests
-    // TODO use @BeforeEach and other best practice things in Tests
-    // TODO Controller util classes
-    // TODO Correct ErrorMessage return in RestController (Mock and integration test)
-    // TODO Correct Json of Rest Controller [Rest Level 3] (Mock tests only)
-    // TODO Event Tests
+// TODO [tzaika] Implement Dimension Tests
+// TODO [tzaika] Implement Impact Tests
+// TODO Cover all scenarios with Tests
+// TODO use @BeforeEach and other best practice things in Tests
+// TODO Controller util classes
+// TODO Correct ErrorMessage return in RestController (Mock and integration test)
+// TODO Correct Json of Rest Controller [Rest Level 3] (Mock tests only)
+// TODO Event Tests
 
 // TODO [tzaika] Test if real events are asynchronous
 
