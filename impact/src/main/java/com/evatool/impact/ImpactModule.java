2cfbe3c0f99608a87e186b4fa48de8f63e3e5e82
Henning Buhl <henning.buhl@yahoo.de>
2021-02-07 11:33:41 +0100

    Added TODOs

--- impact/src/main/java/com/evatool/impact/ImpactModule.java
+++ impact/src/main/java/com/evatool/impact/ImpactModule.java
@@ -3,14 +3,18 @@ package com.evatool.impact;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// TODO Tests for Rest level 3 links
+// TODO Implement error handling for most cases (return helpful errors and links to help the frontend developers)
+// TODO Tests for Rest level 3 links (Uri classes and RestLevel3LinkRel)
+// TODO More tests for Rest Controller - both ServiceMock and Integration test (validate error message, Json and Rest Level 3 Links)
 
-// TODO [tzaika] Implemenet Dimension Tests
+// TODO [tzaika] Implement Dimension Tests
 // TODO [tzaika] Implement Impact Tests
 // TODO [tzaika] Test if real events are asynchronous
 
 // TODO [tzaika & hbuhl] API Specification
 // TODO [tzaika & hbuhl] Eventing Specification
+
+// TODO Implement events and tests
 
 @SpringBootApplication
 public class ImpactModule {
