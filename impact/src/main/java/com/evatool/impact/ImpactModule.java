74a227b1101b96694efd2d1027245f101a4b39b6
Henning Buhl <henning.buhl@yahoo.de>
2021-02-10 18:06:47 +0100

    Update ImpactModule.java

--- impact/src/main/java/com/evatool/impact/ImpactModule.java
+++ impact/src/main/java/com/evatool/impact/ImpactModule.java
@@ -4,21 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // Features:
-// TODO Better error handling for possible rest calls
 // TODO Events
 
 // Tests:
 // TODO [tzaika] Implement Impact Tests
-
-// TODO ErrorMessage Tests
-// TODO Controller util classes
-// TODO JsonMapper Tests
 // TODO Event Tests
 
 // General:
 // TODO [hbuhl] Split GitHub Action into unit and integration tests?
 // TODO [hbuhl] use real database instead of in-memory?
-
 // TODO [tzaika] Test if real events are asynchronous
 // TODO [tzaika] ImpactRest with Swagger
 
