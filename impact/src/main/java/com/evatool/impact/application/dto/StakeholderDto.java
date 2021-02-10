4cda06f13933acf43af248f5051d35c3ca21a997
tzaika <taraszajika@gmail.com>
2021-02-10 18:33:41 +0100

    update on impact data-flow

--- impact/src/main/java/com/evatool/impact/application/dto/StakeholderDto.java
+++ impact/src/main/java/com/evatool/impact/application/dto/StakeholderDto.java
@@ -1,7 +1,5 @@
 package com.evatool.impact.application.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
