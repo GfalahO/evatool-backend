1efe45a029e3284da7c5097b0071b9d10fa4d9ab
JannisLuechtefeld <jannis.luechtefeld@yahoo.de>
2021-02-12 13:44:21 +0100

    eventlistener

--- requirements/src/main/java/com/evatool/requirements/events/listener/RequirementEventListener.java
+++ requirements/src/main/java/com/evatool/requirements/events/listener/RequirementEventListener.java
@@ -1,5 +1,6 @@
 package com.evatool.requirements.events.listener;
 
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -7,32 +8,33 @@ import org.springframework.stereotype.Component;
 public class RequirementEventListener {
 
     @EventListener
-    public void variantCreated(){
+    public void variantCreated(ApplicationEvent event){
+
         //todo
     }
 
     @EventListener
-    public void variantUpdated(){
+    public void variantUpdated(ApplicationEvent event){
         //todo
     }
 
     @EventListener
-    public void variantDeleted(){
+    public void variantDeleted(ApplicationEvent event){
         //todo
     }
 
     @EventListener
-    public void impactCreated(){
+    public void impactCreated(ApplicationEvent event){
         ///todo
     }
 
     @EventListener
-    public void impactUpdated(){
+    public void impactUpdated(ApplicationEvent event){
         //todo
     }
 
     @EventListener
-    public void impactDeleted(){
+    public void impactDeleted(ApplicationEvent event){
         /// TODO
     }
 
