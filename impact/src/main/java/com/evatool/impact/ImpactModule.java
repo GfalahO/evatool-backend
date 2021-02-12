a15ea3e7a4e8a848d804035bacbdc7f0aecc4fbe
Henning Buhl <henning.buhl@yahoo.de>
2021-02-12 15:58:05 +0100

    Added TODOs

--- impact/src/main/java/com/evatool/impact/ImpactModule.java
+++ impact/src/main/java/com/evatool/impact/ImpactModule.java
@@ -11,14 +11,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // TODO [hbuhl] ErrorMessage tests in RestController?
 // TODO [hbuhl & tzaika] Event Tests
 
-// General:
-// TODO use logger: Logger logger = LoggerFactory.getLogger(MyClass.class);
+// Team:
+// TODO [hbuhl & tzaika] use logger: Logger logger = LoggerFactory.getLogger(MyClass.class);
+// TODO [hbuhl & tzaika] Use UUID class in Entity?
+// TODO [hbuhl & tzaika] Keep id in DTO? (Rest Level 3 HATEOAS)
+// TODO [hbuhl] Use poc-way of creating and inserting Rest Level 3 links
 // TODO [hbuhl & tzaika] Test if real events are asynchronous
 // TODO [tzaika] ImpactRest with Swagger
+// TODO [tzaika] use new wireframe in impact wiki
+// TODO [hbuhl & tzaika] GitHub Issues in backend repo
+// TODO [hbuhl & tzaika] Clean dependencies and put in root pom
+// TODO [hbuhl & tzaika] add swagger API documentation (@Api, @ApiOperation, @ApiResponses, @ApiModel, @ApiModelProperty, @ApiParam)
+// TODO [hbuhl] use strings in request mappings for readability
+// TODO [hbuhl & tzaika] Auth - @RolesAllowed
+// TODO [hbuhl] Mandantenfähigkeit? (just for fun)
+// TODO Install SonarLint IDE plugin
+
+// SIG:
+// TODO [hbuhl & tzaika] Build Tool Chain (bis 19.02.2021)
+// TODO [hbuhl] Redo Event Testing (after async test behaviour has been validated in real application)
 
 @SpringBootApplication
 public class ImpactModule {
-	public static void main(String[] args) {
-		SpringApplication.run(ImpactModule.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ImpactModule.class, args);
+    }
 }
