6ef8c2c45fe6fcd1279f464b864c1704c00c7e18
Henning Buhl <henning.buhl@yahoo.de>
2021-02-13 11:00:15 +0100

    TODOs

--- impact/src/main/java/com/evatool/impact/ImpactModule.java
+++ impact/src/main/java/com/evatool/impact/ImpactModule.java
@@ -14,17 +14,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // Team:
 // TODO Install SonarLint IDE plugin and remove warnings
-// TODO [hbuhl] Use poc-way of creating and inserting Rest Level 3 links
 // TODO [hbuhl & tzaika] Test if real events are asynchronous
 // TODO [tzaika] ImpactRest with Swagger
 // TODO [tzaika] use new wireframe in impact wiki
 // TODO [hbuhl & tzaika] GitHub Issues in backend repo: https://github.com/EVATool/evatool-backend/labels/Team%20Impact
 // TODO [hbuhl & tzaika] Clean dependencies and put in root pom
-// TODO [hbuhl & tzaika] add swagger API documentation (@Api, @ApiOperation, @ApiResponses, @ApiModel, @ApiModelProperty, @ApiParam)
+// TODO [tzaika] add swagger API documentation (@Api, @ApiOperation, @ApiResponses, @ApiModel, @ApiModelProperty, @ApiParam)
 // TODO [hbuhl] Mandantenfähigkeit? (just for fun)
 
 // TODO [hbuhl] use strings in request mappings for readability (wait until 19.02.2021) [Property file?]
-// TODO [hbuhl & tzaika] Use UUID class in Entity? (wait until 19.02.2021)
 // TODO [hbuhl & tzaika] Keep id in DTO? (Rest Level 3 HATEOAS) (wait until 19.02.2021)
 // TODO [hbuhl & tzaika] Auth - @RolesAllowed (wait until 19.02.2021)
 
