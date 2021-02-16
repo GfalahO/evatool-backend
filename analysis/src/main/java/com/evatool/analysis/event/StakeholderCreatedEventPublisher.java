cfb5052be766339b0d7a0fbcdc8e2c989161e3f2
Henning Buhl <henning.buhl@yahoo.de>
2021-02-16 21:22:00 +0100

    Real events in application are async, tests are sync

--- analysis/src/main/java/com/evatool/analysis/event/StakeholderCreatedEventPublisher.java
+++ analysis/src/main/java/com/evatool/analysis/event/StakeholderCreatedEventPublisher.java
@@ -4,6 +4,8 @@ import com.evatool.global.event.stakeholder.StakeholderCreatedEvent;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 public class StakeholderCreatedEventPublisher {
     private ApplicationEventPublisher applicationEventPublisher;
@@ -12,8 +14,14 @@ public class StakeholderCreatedEventPublisher {
         this.applicationEventPublisher = applicationEventPublisher;
     }
 
-    public void onStakeholderCreated() {
-        var stakeholderCreatedEvent = new StakeholderCreatedEvent(this, "Test Payload");
+    public void onStakeholderCreated() throws InterruptedException {
+        var json = String.format("{\"id\":\"%s\",\"name\":\"%s\"}", UUID.randomUUID().toString(), "ich komme aus analysis");
+
+        var stakeholderCreatedEvent = new StakeholderCreatedEvent(this, json);
         applicationEventPublisher.publishEvent(stakeholderCreatedEvent);
+        for (int i = 0; i < 10; i++) {
+            Thread.sleep(100);
+            System.out.println("PUBLISHER");
+        }
     }
 }
--- impact/src/main/java/com/evatool/impact/domain/event/stakeholder/StakeholderCreatedEventListener.java
+++ impact/src/main/java/com/evatool/impact/domain/event/stakeholder/StakeholderCreatedEventListener.java
@@ -3,6 +3,7 @@ package com.evatool.impact.domain.event.stakeholder;
 import com.evatool.global.event.stakeholder.StakeholderCreatedEvent;
 import com.evatool.impact.application.json.mapper.ImpactStakeholderJsonMapper;
 import com.evatool.impact.domain.repository.ImpactStakeholderRepository;
+import lombok.SneakyThrows;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
@@ -15,14 +16,15 @@ public class StakeholderCreatedEventListener implements ApplicationListener<Stak
         this.stakeholderRepository = stakeholderRepository;
     }
 
+    @SneakyThrows
     @Override
     public void onApplicationEvent(final StakeholderCreatedEvent event) {
         var jsonPayload = event.getJsonPayload();
         var stakeholder = ImpactStakeholderJsonMapper.fromJson(jsonPayload);
-        System.out.println(stakeholder);
-        System.out.println(stakeholderRepository.save(stakeholder));
-        //stakeholderRepository.save(stakeholder);
-        System.out.println(stakeholder.getId());
-        System.out.println(stakeholderRepository.findById(stakeholder.getId()).orElse(null));
+        stakeholderRepository.save(stakeholder);
+        for (int i = 0; i < 10; i++) {
+            Thread.sleep(100);
+            System.out.println("LISTENER: " + jsonPayload);
+        }
     }
 }
