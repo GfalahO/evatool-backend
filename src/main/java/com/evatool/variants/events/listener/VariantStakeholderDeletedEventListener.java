da4006567fabd403fd96b99102e7a98969afb6ae
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-02-26 13:04:47 +0100

    Implemented Eventlistener

--- src/main/java/com/evatool/variants/events/listener/VariantStakeholderDeletedEventListener.java
+++ src/main/java/com/evatool/variants/events/listener/VariantStakeholderDeletedEventListener.java
@@ -1,4 +1,24 @@
 package com.evatool.variants.events.listener;
 
-public class VariantStakeholderDeletedEventListener {
+import com.evatool.global.event.analysis.AnalysisDeletedEvent;
+import com.evatool.global.event.stakeholder.StakeholderCreatedEvent;
+import com.evatool.global.event.stakeholder.StakeholderDeletedEvent;
+import com.evatool.variants.entities.VariantsStakeholder;
+import com.evatool.variants.repositories.VariantsStakeholderRepository;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VariantStakeholderDeletedEventListener implements ApplicationListener<StakeholderDeletedEvent> {
+    @Autowired
+    VariantsStakeholderRepository variantsStakeholderRepository;
+    Gson gson = new Gson();
+
+    @Override
+    public void onApplicationEvent(StakeholderDeletedEvent stakeholderDeletedEvent) {
+        VariantsStakeholder variantsStakeholder = gson.fromJson(stakeholderDeletedEvent.getJsonPayload(), VariantsStakeholder.class);
+        variantsStakeholderRepository.delete(variantsStakeholder);
+    }
 }
