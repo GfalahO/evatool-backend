73e78f7c39b937e80d1f50fd281f67ce6978ffe5
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-02-26 13:04:27 +0100

    Implemented Eventlistener

--- src/main/java/com/evatool/variants/events/listener/VariantAnalysisDeletedEventListener.java
+++ src/main/java/com/evatool/variants/events/listener/VariantAnalysisDeletedEventListener.java
@@ -1,4 +1,24 @@
 package com.evatool.variants.events.listener;
 
-public class VariantAnalysisDeletedEventListener {
+import com.evatool.global.event.analysis.AnalysisCreatedEvent;
+import com.evatool.global.event.analysis.AnalysisDeletedEvent;
+import com.evatool.variants.entities.VariantsAnalysis;
+import com.evatool.variants.repositories.VariantsAnalysisRepository;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VariantAnalysisDeletedEventListener implements ApplicationListener<AnalysisDeletedEvent> {
+    @Autowired
+    VariantsAnalysisRepository variantsAnalysisRepository;
+    Gson gson = new Gson();
+
+    @Override
+    public void onApplicationEvent(AnalysisDeletedEvent analysisDeletedEvent) {
+        // TODO uncomment once the event has been correctly implemented
+        // VariantsAnalysis variantsAnalysis = gson.fromJson(analysisDeletedEvent.getMessage(), VariantsAnalysis.class);
+        // variantsAnalysisRepository.delete(variantsAnalysis);
+    }
 }
