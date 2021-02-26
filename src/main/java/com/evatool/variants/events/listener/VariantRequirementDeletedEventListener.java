110c3df4b731284c87ec2fd9ad63dfc9f9468a9a
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-02-26 13:04:36 +0100

    Implemented Eventlistener

--- src/main/java/com/evatool/variants/events/listener/VariantRequirementDeletedEventListener.java
+++ src/main/java/com/evatool/variants/events/listener/VariantRequirementDeletedEventListener.java
@@ -1,4 +1,23 @@
 package com.evatool.variants.events.listener;
 
-public class VariantRequirementDeletedEventListener {
+import com.evatool.global.event.analysis.AnalysisDeletedEvent;
+import com.evatool.global.event.requirements.RequirementDeletedEvent;
+import com.evatool.variants.entities.VariantsRequirement;
+import com.evatool.variants.repositories.VariantsRequirementRepository;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VariantRequirementDeletedEventListener implements ApplicationListener<RequirementDeletedEvent> {
+    @Autowired
+    VariantsRequirementRepository variantsRequirementRepository;
+    Gson gson = new Gson();
+
+    @Override
+    public void onApplicationEvent(RequirementDeletedEvent requirementDeletedEvent) {
+        VariantsRequirement variantsRequirement = gson.fromJson(requirementDeletedEvent.getJsonPayload(), VariantsRequirement.class);
+        variantsRequirementRepository.delete(variantsRequirement);
+    }
 }
