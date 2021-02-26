d0c10dc7e1314ccd3fc836d68c9712c4e0496968
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-02-26 18:28:33 +0100

    Fixed incorrect null handling

--- src/main/java/com/evatool/variants/services/VariantMapper.java
+++ src/main/java/com/evatool/variants/services/VariantMapper.java
@@ -39,28 +39,41 @@ public class VariantMapper {
         variantDto.setTitle(variant.getTitle());
         variantDto.setCriterion(variant.getCriterion());
 
-        Link stakeholderLink = linkTo(methodOn(StakeholderController.class).getStakeholderList()).withSelfRel();
-        EntityModel<VariantsStakeholder> variantsStakeholders = EntityModel.of(variant.getVariantsStakeholder());
-        variantsStakeholders.add(stakeholderLink);
-
-        variantDto.setVariantsStakeholder(EntityModel.of(variant.getVariantsStakeholder()));
+        if (variant.getVariantsStakeholder() != null) {
+            Link stakeholderLink = linkTo(methodOn(StakeholderController.class).getStakeholderList()).withSelfRel();
+            EntityModel<VariantsStakeholder> variantsStakeholder = EntityModel.of(variant.getVariantsStakeholder());
+            variantsStakeholder.add(stakeholderLink);
+            variantDto.setVariantsStakeholder(variantsStakeholder);
+        }
 
         variantDto.setDescription(variant.getDescription());
 
         variantDto.setStFlagsPot(variant.isStFlagsPot());
         variantDto.setStFlagsReal(variant.isStFlagsReal());
 
-        Link subVariantLink = linkTo(methodOn(VariantController.class).getAllVariants()).withSelfRel();
-        CollectionModel<Variant> subVariantCollectionModel = CollectionModel.of(new ArrayList<>(variant.getSubVariant()));
-        subVariantCollectionModel.add(subVariantLink);
+        if (variant.getSubVariant() != null){
+            Link subVariantLink = linkTo(methodOn(VariantController.class).getAllVariants()).withSelfRel();
+            List<Variant> variantList = new ArrayList<>();
+            variantList.addAll(variant.getSubVariant());
+            CollectionModel<Variant> subVariantCollectionModel = CollectionModel.of(variantList);
+            subVariantCollectionModel.add(subVariantLink);
+        }
 
-        Link variantAnalysisLink = linkTo(methodOn(AnalysisController.class).getAnalysisList()).withSelfRel();
-        CollectionModel<VariantsAnalysis> analysisCollectionModel = CollectionModel.of(variant.getVariantsAnalyses());
-        analysisCollectionModel.add(variantAnalysisLink);
+        if (variant.getVariantsAnalyses() != null){
+            Link variantAnalysisLink = linkTo(methodOn(AnalysisController.class).getAnalysisList()).withSelfRel();
+            List<VariantsAnalysis> variantsAnalysisList = new ArrayList<>();
+            variantsAnalysisList.addAll(variant.getVariantsAnalyses());
+            CollectionModel<VariantsAnalysis> analysisCollectionModel = CollectionModel.of(variantsAnalysisList);
+            analysisCollectionModel.add(variantAnalysisLink);
+        }
 
-        Link variantRequirementLink = linkTo(methodOn(RequirementsController.class).getRequirementList()).withSelfRel();
-        CollectionModel<VariantsRequirement> variantsRequirementCollectionModel = CollectionModel.of(variant.getVariantsRequirements());
-        variantsRequirementCollectionModel.add(variantRequirementLink);
+        if (variant.getVariantsRequirements() != null) {
+            Link variantRequirementLink = linkTo(methodOn(RequirementsController.class).getRequirementList()).withSelfRel();
+            List<VariantsRequirement> variantsRequirementsList = new ArrayList<>();
+            variantsRequirementsList.addAll(variant.getVariantsRequirements());
+            CollectionModel<VariantsRequirement> variantsRequirementCollectionModel = CollectionModel.of(variantsRequirementsList);
+            variantsRequirementCollectionModel.add(variantRequirementLink);
+        }
 
         return variantDto;
     }
@@ -70,13 +83,20 @@ public class VariantMapper {
         variant.setId(variantDto.getUuid());
         variant.setTitle(variantDto.getTitle());
         variant.setCriterion(variantDto.getCriterion());
-        variant.setSubVariant(variantDto.getSubVariant().getContent().stream().collect(Collectors.toList()));
+        if(variantDto.getSubVariant() != null) {
+            variant.setSubVariant(variantDto.getSubVariant().getContent().stream().collect(Collectors.toList()));
+        }
         variant.setDescription(variantDto.getDescription());
         variant.setStFlagsPot(variantDto.isStFlagsPot());
         variant.setStFlagsReal(variantDto.isStFlagsReal());
         variant.setVariantsStakeholder(variantDto.getVariantsStakeholder().getContent());
-        variant.setVariantsAnalyses(variantDto.getVariantsAnalyses().getContent().stream().collect(Collectors.toList()));
-        variant.setVariantsRequirements(variantDto.getVariantsRequirements().getContent().stream().collect(Collectors.toList()));
+        if(variantDto.getVariantsAnalyses() != null) {
+            variant.setVariantsAnalyses(variantDto.getVariantsAnalyses().getContent().stream().collect(Collectors.toList()));
+        }
+        if (variantDto.getVariantsRequirements() != null){
+            variant.setVariantsRequirements(variantDto.getVariantsRequirements().getContent().stream().collect(Collectors.toList()));
+        }
+
 
         return variant;
     }
