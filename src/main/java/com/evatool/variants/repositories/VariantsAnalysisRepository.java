1d18871c8deee142b81dda50dfe139591f8553ed
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-02-26 13:03:42 +0100

    Fixed Typo in VariantsAnalysis Classname

--- src/main/java/com/evatool/variants/repositories/VariantsAnalysisRepository.java
+++ src/main/java/com/evatool/variants/repositories/VariantsAnalysisRepository.java
@@ -1,13 +1,12 @@
 package com.evatool.variants.repositories;
 
-import com.evatool.variants.entities.VariantAnalysis;
+import com.evatool.variants.entities.VariantsAnalysis;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface VariantsAnalysisRepository extends JpaRepository<VariantAnalysis, UUID> {
+public interface VariantsAnalysisRepository extends JpaRepository<VariantsAnalysis, UUID> {
 
 }
