60cb99a17559258f8bced8015d4c3cc35f762f88
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-02-26 13:04:39 +0100

    Implemented Eventlistener

--- src/main/java/com/evatool/variants/events/listener/VariantRequirementUpdatedEventListener.java
+++ src/main/java/com/evatool/variants/events/listener/VariantRequirementUpdatedEventListener.java
@@ -1,4 +1,23 @@
 package com.evatool.variants.events.listener;
 
-public class VariantRequirementUpdatedEventListener {
+import com.evatool.global.event.analysis.AnalysisDeletedEvent;
+import com.evatool.global.event.requirements.RequirementUpdatedEvent;
+import com.evatool.variants.entities.VariantsRequirement;
+import com.evatool.variants.repositories.VariantsRequirementRepository;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VariantRequirementUpdatedEventListener implements ApplicationListener<RequirementUpdatedEvent> {
+    @Autowired
+    VariantsRequirementRepository variantsRequirementRepository;
+    Gson gson = new Gson();
+
+    @Override
+    public void onApplicationEvent(RequirementUpdatedEvent requirementUpdatedEvent) {
+        VariantsRequirement variantsRequirement = gson.fromJson(requirementUpdatedEvent.getJsonPayload(), VariantsRequirement.class);
+        variantsRequirementRepository.save(variantsRequirement);
+    }
 }
