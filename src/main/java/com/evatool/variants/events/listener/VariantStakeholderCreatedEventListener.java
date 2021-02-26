911bf0320828ccc4cc278ad5900f180d7678d9e0
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-02-26 13:04:41 +0100

    Implemented Eventlistener

--- src/main/java/com/evatool/variants/events/listener/VariantStakeholderCreatedEventListener.java
+++ src/main/java/com/evatool/variants/events/listener/VariantStakeholderCreatedEventListener.java
@@ -1,4 +1,23 @@
 package com.evatool.variants.events.listener;
 
-public class VariantStakeholderCreatedEventListener {
+import com.evatool.global.event.analysis.AnalysisDeletedEvent;
+import com.evatool.global.event.stakeholder.StakeholderCreatedEvent;
+import com.evatool.variants.entities.VariantsStakeholder;
+import com.evatool.variants.repositories.VariantsStakeholderRepository;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VariantStakeholderCreatedEventListener implements ApplicationListener<StakeholderCreatedEvent> {
+    @Autowired
+    VariantsStakeholderRepository variantsStakeholderRepository;
+    Gson gson = new Gson();
+
+    @Override
+    public void onApplicationEvent(StakeholderCreatedEvent stakeholderCreatedEvent) {
+        VariantsStakeholder variantsStakeholder = gson.fromJson(stakeholderCreatedEvent.getJsonPayload(), VariantsStakeholder.class);
+        variantsStakeholderRepository.save(variantsStakeholder);
+    }
 }
